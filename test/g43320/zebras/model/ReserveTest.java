7c233fcf038951a9941af4d6861a813056c893ec
s_u_y_s_a <s_u_y_s_a@guest-dyn-ip-182.wlg.esi.heb.site>
2017-03-21 21:48:09 +0100

    Unused export removed in ReserveTest.java

--- test/g43320/zebras/model/ReserveTest.java
+++ test/g43320/zebras/model/ReserveTest.java
@@ -1,9 +1,5 @@
 package g43320.zebras.model;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
