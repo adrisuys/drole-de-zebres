ca2046c12ee4f2f00c534f7ce736dae856eb777e
s_u_y_s_a <s_u_y_s_a@guest-dyn-ip-227.wlg.esi.heb.site>
2017-03-31 10:46:14 +0200

    Remise 2 : working on view (still) : reserve display done

--- src/g43320/zebras/view/Display.java
+++ src/g43320/zebras/view/Display.java
@@ -2,10 +2,12 @@ package g43320.zebras.view;
 
 import g43320.zebras.model.Animal;
 import g43320.zebras.model.Color;
+import g43320.zebras.model.Coordinates;
 import g43320.zebras.model.ImpalaJones;
 import g43320.zebras.model.Pieces;
 import g43320.zebras.model.Reserve;
 import g43320.zebras.model.Species;
+import java.util.Scanner;
 
 /**
  * Handle all the displays for the game.
@@ -77,5 +79,70 @@ public class Display {
  
         //display
         System.out.println(aString);
+    }
+
+    public static int askDistance () {
+        Scanner clavier = new Scanner(System.in);
+        int distance;
+        System.out.println("Give the distance you want Impala Jones to walk, must be a 1, 2 or 3");
+        distance = clavier.nextInt();
+        return distance;
+    }
+    
+    public static Animal chooseAnimalFromStock () {
+        Color color;
+        Species species;
+        Animal animal;
+        Scanner clavier = new  Scanner(System.in);
+        System.out.println("Choose a color between 'RED' or 'GREEN'");
+        String colorUser = clavier.next();
+        while (!colorUser.equals("RED") || !colorUser.equals("GREEN")) {
+            System.out.println("Choose a color between 'RED' or 'GREEN'");
+            colorUser = clavier.next();
+        }
+        if (colorUser.equals("RED")) {
+            color = Color.RED;
+        } else {
+            color = Color.GREEN;
+        }
+        
+        System.out.println("Choose a animal between ELEPHANT, LION, ZEBRA, GAZELLE AND CROCODILE");
+        String speciesUser = clavier.next();
+        while (!speciesUser.equals("ZEBRA") || !speciesUser.equals("GAZELLE") || !speciesUser.equals("ELEPHANT") || !speciesUser.equals("CROCODILE") || !speciesUser.equals("LION")) {
+            System.out.println("Choose a animal between ELEPHANT, LION, ZEBRA, GAZELLE AND CROCODILE");
+            colorUser = clavier.next();
+        }
+        switch (colorUser) {
+            case "GAZELLE" : species = Species.GAZELLE; break;
+            case "ZEBRA" : species = Species.ZEBRA; break;
+            case "ELEPHANT" : species = Species.ELEPHANT; break;
+            case "LION" : species = Species.LION; break;
+            default : species = Species.CROCODILE; break;
+        }
+        
+        animal = new Animal(species,color);
+        return animal;
+        
+    }
+
+    public static Coordinates chooseCoordinates () {
+        int row;
+        int column;
+        Scanner clavier = new  Scanner(System.in);
+        Coordinates position;
+        System.out.println("Choose a row");
+        row = clavier.nextInt();
+        while (row<0 || row>4) {
+            System.out.println("Choose a row");
+        row = clavier.nextInt();
+        }
+        System.out.println("Choose a column");
+        column = clavier.nextInt();
+        while (column<0 || column>4) {
+            System.out.println("Choose a column");
+        column = clavier.nextInt();
+        }
+        position = new Coordinates (row,column);
+        return position;
     }
 }
